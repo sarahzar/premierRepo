b0b6ff92c485ecd99b8aaa8041670cd12b2577e8
MarineLC <49683302+MarineLC@users.noreply.github.com>
2019-04-16 16:17:27 +0200

    commit marine

--- src/main/java/spring/exemples/premierTestSpring/App.java
+++ src/main/java/spring/exemples/premierTestSpring/App.java
@@ -14,7 +14,7 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+    	//commit marine
     	//test proj distant
        ApplicationContext ctx=new ClassPathXmlApplicationContext("config.xml");
        serviceInterface service=(serviceInterface) ctx.getBean("service");
